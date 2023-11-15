19e0ab845ed719dbd5b6e7ac61b9686c2f84907b
daniel-js0 <danielolagbegi1@gmail.com>
2023-11-15 12:15:26 +0100

    updated

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,14 +6,21 @@ const Navbar = () => {
   return (
     <main>
 
-        <div className='flex justify-evenly mt-2'>
+        <div className='flex justify-around lg:justify-evenly mt-2'>
+            <svg className='flex lg:hidden' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path d="M3 7H21" stroke="#39B7B3" stroke-width="1.5" stroke-linecap="round"/>
+                <path d="M3 12H21H3Z" fill="#39B7B3"/>
+                <path d="M3 12H21" stroke="#39B7B3" stroke-width="1.5" stroke-linecap="round"/>
+                <path d="M3 17H21H3Z" fill="#39B7B3"/>
+                <path d="M3 17H21" stroke="#39B7B3" stroke-width="1.5" stroke-linecap="round"/>
+            </svg>
             <div className='flex'>
             <h1 className='mt-1'>FREED CAPITALS</h1>
             <Image src="/Logo.png" width={30} height={10} quality={100} alt="freedcapital logo" placeholder="empty"
                    className='ml-2'  />      
             </div>
 
-            <div>
+            <div className='hidden lg:flex'>
                 <Link href="/" >
                     About
                 </Link>
@@ -31,7 +38,7 @@ const Navbar = () => {
                 </Link>
             </div>
 
-            <button className='h-8 w-24 text-[#39B7B380] border border-[#39B7B380] rounded-3xl'>
+            <button className='hidden lg:flex h-8 w-24 text-[#39B7B380] border border-[#39B7B380] rounded-3xl'>
                 Contact
             </button>
 
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,6 @@
-import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,10 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar/>
         {children}
-        </body>
+        
+      </body>
     </html>
   )
 }
