122a30701dba99438e1fec66ecc5a653f07ac46f
daniel-js0 <danielolagbegi1@gmail.com>
2023-11-16 13:56:35 +0100

    updated

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -17,31 +17,31 @@ const Navbar = () => {
                 </svg>
             </div>
             <div className='flex mr-5'>
-            <h1 className='lg:mt-5 mt-3 tracking-wider font-semibold'>FREED CAPITALS</h1>
-            <Image src="/Logo.png" width={30} height={10} quality={100} alt="freedcapital logo" placeholder="empty"
-                   className='ml-2 h-8 lg:mt-4 mt-2'  />      
+            <h1 className='lg:mt-3 mt-3 text-2xl tracking-widest font-medium'>FREED CAPITALS</h1>
+            <Image src="/Logo.png" width={27} height={10} quality={100} alt="freedcapital logo" placeholder="empty"
+                   className='ml-2 h-7 lg:mt-4 mt-2'  />      
             </div>
 
             <div className='hidden lg:flex mt-5'>
-                <Link href="/" >
+                <Link href="/" className=' text-lg font-light text-decoration'>
                     About
                 </Link>
-                <Link href="/" className='ml-6'>
+                <Link href="/" className='ml-16 text-lg font-light'>
                     Industries
                 </Link>
-                <Link href="/" className='ml-6'>
+                <Link href="/" className='ml-16 text-lg font-light'>
                     Solutions
                 </Link>
-                <Link href="/" className='ml-6'>
+                <Link href="/" className='ml-16 text-lg font-light'>
                     Insight
                 </Link>
-                <Link href="/" className='ml-6'>
+                <Link href="/" className='ml-16 text-lg font-light'>
                     Careers
                 </Link>
             </div>
 
-            <button className='mt-4 hidden  lg:flex h-8 w-24 text-[#39B7B380] border border-[#39B7B380] rounded-3xl'>
-                <p className='mx-auto'>Contact</p>
+            <button className='mt-4 hidden  lg:flex h-8 w-28 text-[#39B7B3] border border-[#39B7B3] rounded-3xl'>
+                <p className='mx-auto mt-0.5 '>Contact</p>
             </button>
 
 
