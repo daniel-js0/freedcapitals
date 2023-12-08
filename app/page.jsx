54c92bab35bcbb61b3fc0a2c27a576a3b3c0615b
daniel-js0 <danielolagbegi1@gmail.com>
2023-12-08 21:24:44 +0100

    updated

--- app/page.jsx
+++ app/page.jsx
@@ -57,14 +57,24 @@ export default function Home() {
       <h1 className="text-5xl font-bold mt-28 px-20 leading-snug tracking-wider">Here are some of the <br /> industries we feature.</h1>
       
       <p className="px-20 text-xl mt-5 font-semibold text-gray-400 tracking-wider ">We collaborate with players in various industries for lasting <br /> achievement, spanning finance, healthcare, technology, and <br /> agriculture. Our objective is to achieve excellence and progress <br /> consistently.</p>
-     
-      <div className="mt-5 h-12 w-56 ">
-      <button className=" h-12 w-56 border font-medium rounded-2xl text-[#39B7B3] border-[#39B7B3]">Energy & Natural Resources</button>
-      </div> 
+      <div className="flex">
+        <div className="mt-5 h-12 w-56 ">
+        <button className=" h-12 w-56 border font-medium rounded-2xl text-[#39B7B3] border-[#39B7B3]">Energy & Natural Resources</button>
+        </div> 
+Energy & Natural Resources
+
+
+        <div className="mt-5 h-12 w-56 ">
+        <button className=" h-12 w-56 border font-medium rounded-2xl text-[#39B7B3] border-[#39B7B3]">IT & Media</button>
+        </div> 
+        <div className="mt-5 h-12 w-56 ">
+        <button className=" h-12 w-56 border font-medium rounded-2xl text-[#39B7B3] border-[#39B7B3]">Oil & Gas</button>
+        </div> 
+      </div>
       </div>
      
       <div>
-      <Image className=' w-[10%] h-[5rems] mx-auto absolute mt-28 '  src="https://www.freedcapitals.com/Images/pg2-bg-icon.svg" width={227} height={210} quality={100} alt="freedcapital logo" placeholder="empty"  /> 
+      <Image className=' w-[10%] h-[5rems] mx-auto absolute mt-24 '  src="https://www.freedcapitals.com/Images/pg2-bg-icon.svg" width={227} height={210} quality={100} alt="freedcapital logo" placeholder="empty"  /> 
      
       <Image className=' w-[28rem] h-[32rem] relative  mt-20'  src="https://www.freedcapitals.com/Images/c-hero.svg" width={427} height={510} quality={100} alt="freedcapital logo" placeholder="empty"  /> 
       </div>
