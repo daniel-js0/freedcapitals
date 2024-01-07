233c139c1f1d6797a7be43e083558fb563465637
daniel-js0 <danielolagbegi1@gmail.com>
2024-01-07 16:49:41 +0100

    update

--- app/industries/page.jsx
+++ app/industries/page.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import Image from "next/image";
 
 const Industries = () => {
   return (
     <main>
-      <div>
-        <h1>
-            
-        </h1>
+      <div className='w-[100%] h-[70%] relative'>
+        <Image className=' w-[100%] h-[100%] mx-auto '  src="https://www.freedcapitals.com/Images/Frame-184.svg" width={227} height={210} quality={100} alt="freedcapital logo" placeholder="empty"  /> 
+        <h1 className='text-3xl font-semibold absolute text-slate-200 top-0'>We collaborate with Various industries to provide tailored solution to our clients</h1>
+      
       </div>
+       <Image className=''  src="https://www.freedcapitals.com/Images/Group.svg" width={227} height={210} quality={100} alt="freedcapital logo" placeholder="empty"  /> 
+
 
     </main>
   )
