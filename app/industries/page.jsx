220473fa2be4d987603623b67bbc445cfd339424
daniel-js0 <danielolagbegi1@gmail.com>
2024-01-08 21:01:04 +0100

    update

--- app/industries/page.jsx
+++ app/industries/page.jsx
@@ -6,7 +6,7 @@ const Industries = () => {
     <main>
       <div className='w-[100%] h-[70%] relative'>
         <Image className=' w-[100%] h-[100%] mx-auto '  src="https://www.freedcapitals.com/Images/Frame-184.svg" width={227} height={210} quality={100} alt="freedcapital logo" placeholder="empty"  /> 
-        <h1 className='text-3xl font-semibold absolute text-slate-200 top-0'>We collaborate with Various industries to provide tailored solution to our clients</h1>
+        <h1 className='text-5xl px-20 mt-44 font-semibold absolute text-slate-200 top-0'>We collaborate with Various industries to provide tailored solution to our clients</h1>
       
       </div>
        <Image className=''  src="https://www.freedcapitals.com/Images/Group.svg" width={227} height={210} quality={100} alt="freedcapital logo" placeholder="empty"  /> 
